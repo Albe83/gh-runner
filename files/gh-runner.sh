510778e1132ba774f28c27057643b3a312214a47
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2025-08-22 19:09:34 +0000

    Aggiungi la rimozione del token GITHUB_TOKEN dopo l'esecuzione di run.sh per migliorare la sicurezza dello script

--- files/gh-runner.sh
+++ files/gh-runner.sh
@@ -7,5 +7,5 @@
     --url "${GITHUB_URL}" \
     --token "${GITHUB_TOKEN}" \
     --labels "${GITHUB_LABELS}" \
-&& GITHUB_TOKEN="" ./run.sh
+&& GITHUB_TOKEN="" ./run.sh \
 && ./config.sh remove --token "${GITHUB_TOKEN}"
